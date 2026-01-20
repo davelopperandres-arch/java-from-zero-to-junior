65fc36c66359ba6f3ff03c17bb3638ba1c3b8e54
Wilches13 <wilcheshotmart@gmail.com>
2026-01-20 12:08:03 -0500

    FINALIZACION, SE AGREGO CORRECTAMENTE LA FUNCIONALIDAD DE ELIMINAR EMPLEADOS

--- Proyecto 4/SistemaGestionEmpleadosMenu.java
+++ Proyecto 4/SistemaGestionEmpleadosMenu.java
@@ -29,6 +29,9 @@ public class SistemaGestionEmpleadosMenu {
             switch (opcion){
 
                 case 1:
+
+                    RegistroNuevo = true;
+
                     while (RegistroNuevo) {
 
                         entrada.nextLine();
@@ -40,7 +43,7 @@ public class SistemaGestionEmpleadosMenu {
                         String Cargo = entrada.nextLine();
 
                         System.out.println("Ingresar telefono :");
-                        Integer Telefono = entrada.nextInt();
+                        int Telefono = entrada.nextInt();
 
                         System.out.println("Ingresar Salario :");
                         double Salario = entrada.nextDouble();
@@ -60,20 +63,101 @@ public class SistemaGestionEmpleadosMenu {
                         } else {
                             RegistroNuevo = false;
                         }
-
-
                     }
-
                     break;
 
                 case 2:
                     System.out.println("Entrando a base de datos de empleados...");
+                    if (Nombres.size() == 0) {
+                        System.out.println("No hay empleados registrados.");
+                    } else {
+                        System.out.println("--- LISTA DE EMPLEADOS REGISTRADOS ---");
+
+                        for (int i = 0; i < Nombres.size(); i++) {
+                            System.out.println(
+                                    "Empleado " + (i + 1) + ": " +
+                                            Nombres.get(i) + " | " +
+                                            Cargos.get(i) + " | " +
+                                            Telefonos.get(i) + " | $" +
+                                            Salarios.get(i)
+                            );
+                        }
+                    }
                     break;
                 case 3:
-                    System.out.println("Promedio salarial es...");
+
+                    if (Salarios.isEmpty()) {
+                        System.out.println("No hay empleados registrados.");
+                        System.out.println("Primero debe registrar empleados.");
+                    } else {
+
+                        double suma = 0;
+
+                        for (int i = 0; i < Salarios.size(); i++) {
+                            suma += Salarios.get(i);
+                        }
+
+                        double promedio = suma / Salarios.size();
+
+                        System.out.println("===== PROMEDIO SALARIAL =====");
+                        System.out.println("Total empleados: " + Salarios.size());
+                        System.out.println("Promedio salarial: $" + promedio);
+                    }
+
                     break;
+
                 case 4:
-                    System.out.println("Elije el empleado a eliminar...");
+
+                    if (Nombres.size() == 0) {
+                        System.out.println("No hay empleados registrados.");
+                    } else {
+                        System.out.println("--- LISTA DE EMPLEADOS REGISTRADOS ---");
+
+                        for (int i = 0; i < Nombres.size(); i++) {
+                            System.out.println(
+                                    "Empleado " + (i + 1) + ": " +
+                                            Nombres.get(i) + " | " +
+                                            Cargos.get(i) + " | " +
+                                            Telefonos.get(i) + " | $" +
+                                            Salarios.get(i)
+                            );
+
+                        }
+                        System.out.println("INGRESAR EL NUMERO DE EMPLEADO A ELIMINAR :");
+                        int numeroEmpleado = entrada.nextInt();
+
+                        if (numeroEmpleado < 1 || numeroEmpleado > Nombres.size()) {
+                            System.out.println("Numero de empleado invalido.");
+                        } else {
+                            int indice = numeroEmpleado - 1;
+                            boolean eliminado = true;
+
+                            while (eliminado) {
+                                entrada.nextLine();
+                                System.out.println("¿Esta seguro que desea eliminar al empleado " + Nombres.get(indice) +"||" + Cargos.get(indice)+ "? S/N");
+                                String confirmacion = entrada.nextLine();
+
+                                if (confirmacion.equalsIgnoreCase("S")) {
+                                    eliminado = false;
+
+
+                                    Nombres.remove(indice);
+                                    Cargos.remove(indice);
+                                    Telefonos.remove(indice);
+                                    Salarios.remove(indice);
+
+                                    System.out.println("Empleado eliminado exitosamente.");
+
+                                } else if (confirmacion.equalsIgnoreCase("N")) {
+                                    System.out.println("Eliminacion cancelada.");
+                                    eliminado = false;
+                                    break;
+                                } else {
+                                    System.out.println("Respuesta invalida. Por favor ingrese S o N.");
+                                }
+                            }
+                        }
+                    }
                     break;
                 case 5:
                     SistemaActivo = false;
